7481685ee5740816349df973d657ad44682b4ef6
Aryanraj0013 <ar2005raj@gmail.com>
2025-07-31 15:06:29 +0530

    alldone

---
+++ backend/src/db/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+const connect = ()=>{
+    mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error:", err);
+    });
+}
+
+module.exports = connect
